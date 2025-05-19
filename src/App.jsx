55811431dac64ffac4471269f5965f860b7cf001
nurgissa <nrakhymzhan@mtechs.kz>
2025-05-19 17:04:14 +0500

    ref: fixed layout to header

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
         <div className="App flex flex-col items-center bg-light text-dark min-h-screen scroll-smooth">
             <div className="w-full max-w-[1920px]">
                 {/* Навигация */}
-                <header className="bg-white shadow-md border-b">
+                <header className="fixed top-0 left-0 w-full z-50 bg-white shadow-md border-b">
                     <nav className="max-w-7xl mx-auto flex justify-between items-center p-4">
                         <div className="">
-                            <img src={logo} alt="logo" className="w-24" />
+                            <img src={logo} alt="logo" className="w-24"/>
                         </div>
 
                         {/* Десктоп меню */}
@@ -29,7 +29,8 @@ function App() {
 
                         {/* Бургер кнопка */}
                         <div className="md:hidden px-8">
-                            <button onClick={() => setMenuOpen(!menuOpen)} className="focus:outline-none text-secondary">
+                            <button onClick={() => setMenuOpen(!menuOpen)}
+                                    className="focus:outline-none text-secondary">
                                 {menuOpen ? <CloseIcon fontSize="large"/> : <MenuIcon fontSize="large"/>}
                             </button>
                         </div>
@@ -43,7 +44,8 @@ function App() {
                       `}
                     >
                         <a href="#about" className="text-secondary" onClick={() => setMenuOpen(false)}>О нас</a>
-                        <a href="#services" className="text-secondary" onClick={() => setMenuOpen(false)}>Компетенции</a>
+                        <a href="#services" className="text-secondary"
+                           onClick={() => setMenuOpen(false)}>Компетенции</a>
                         <a href="#projects" className="text-secondary" onClick={() => setMenuOpen(false)}>Проекты</a>
                         <a href="#contacts" className="text-secondary" onClick={() => setMenuOpen(false)}>Контакты</a>
                     </div>
@@ -51,7 +53,7 @@ function App() {
                 </header>
 
                 {/* О нас */}
-                <section id="about" className="bg-white mx-auto flex flex-col items-center py-8 px-6 text-center">
+                <section id="about" className="bg-white mx-auto flex flex-col items-center pt-20 py-8 px-6 text-center">
                     <h1 className="text-3xl font-bold mb-6">IT-разработка, которая решает задачи бизнеса</h1>
                     <div className="text-lg max-w-2xl">
                         <p className="text-2xl font-medium text-secondary">Веб. Мобайл. Интеграции. Автоматизация. </p><br />
@@ -105,8 +107,8 @@ function App() {
 
                 {/* Контакты */}
                 <footer id="contacts" className="bg-white text-light py-8 px-4 text-center text-secondary border-t">
-                    <h3 className="text-2xl text-primary font-semibold mb-4">Контакты</h3>
-                    <p className="text-primary">Остались вопросы? Обязательно нас спросите :)</p>
+                    <h3 className="text-2xl font-semibold mb-4">Контакты</h3>
+                    <p>Остались вопросы? Обязательно нас спросите :)</p>
                     <a href="mailto:info@mtechs.kz" className="mt-2 font-semibold text-secondary">info@mtechs.kz</a>
                 </footer>
             </div>
